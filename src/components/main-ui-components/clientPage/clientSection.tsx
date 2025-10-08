e3e1831729d30e8fdeb0bdffc5fef766afa2c50b
Akbar Hafeez <akbarhafeez25@gmail.com>
2025-10-08 13:54:31 +0500

    client page error fixed

--- src/components/main-ui-components/clientPage/clientSection.tsx
+++ src/components/main-ui-components/clientPage/clientSection.tsx
@@ -142,7 +142,7 @@ export default function ClientsSection(): JSX.Element {
       <div className="max-w-7xl mx-auto px-6 lg:px-12">
         {/* Section Heading */}
         <div className="text-center mb-20">
-          <h2 className="text-4xl md:text-5xl font-extrabold text-gray-900">
+          <h2 className="text-4xl md:text-5xl font-extrabold ">
             Empowering Modern Financial & Web3 Innovators
           </h2>
           <p className="mt-4 text-lg text-color max-w-2xl mx-auto">
@@ -165,7 +165,7 @@ export default function ClientsSection(): JSX.Element {
               >
                 {/* Text Content */}
                 <div className="flex-1">
-                  <h3 className="text-4xl font-bold text-color mb-3">
+                  <h3 className="text-4xl font-bold  mb-3">
                     {c.title}
                   </h3>
                   <p className="text-lg text-gray-600 mb-6">{c.subtitle}</p>
